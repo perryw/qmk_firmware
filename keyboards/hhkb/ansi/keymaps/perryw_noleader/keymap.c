f95dc4b37cf258058cc5b6ebab0e2ff01e9e4524
Perry Wong <perry.wong@carta.com>
2020-07-18 18:17:11 -0700

    feat: add DEL to dev layer

--- keyboards/hhkb/ansi/keymaps/perryw_noleader/keymap.c
+++ keyboards/hhkb/ansi/keymaps/perryw_noleader/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer DEV: DEV mode (DEV Fn)
       TODO: Add a cmd/tab function to the developer layer for quick switching between different applications when debugging
       |------+-----+-----+-----+----+----+--------+----------------+--------------+-----------+-------------+-----+-------+-------+-----|
-      |      | F1  | F2  | F3  | F4 | F5 |   F6   |       F7       |     F8       |     F9    |     F10     | F11 |   F12 |       |     |
+      |      | F1  | F2  | F3  | F4 | F5 |   F6   |       F7       |     F8       |     F9    |     F10     | F11 |   F12 |  DEL  | DEL |
       |------+-----+-----+-----+----+----+--------+----------------+--------------+-----------+-------------+-----+-------+-------+-----|
       | CAPS |     |     |     |    |    |        |                |              |           |             |     |       |       |     |
       |------+-----+-----+-----+----+----+--------+----------------+--------------+-----------+-------------+-----+-------+-------+-----|
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [_DEV] = LAYOUT(
-        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DELETE, KC_DELETE,
         KC_CAPSLOCK, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_NAV),
